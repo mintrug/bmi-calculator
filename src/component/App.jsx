d6bd6d9584cdfd31cf6b4be57771f726f071b53d
mintrug <nguyenngocminhtrung280420@gmail.com>
2023-12-11 00:50:09 +0700

    new update

---
+++ src/component/App.jsx
@@ -0,0 +1,59 @@
+import img from '../assets/chart.png'
+
+function App() {
+    return (
+        <div className='container'>
+            <div className='header center'>
+                <h1>BMI Tracker</h1>
+            </div>
+            <div className='bmi-form center'>
+                <div className='input-left inline-block'>
+                    <label htmlFor='weight'>Weight (in kg)</label>
+                    <input id='weight' type='text' />
+                </div>
+                <div className='input-right inline-block'>
+                    <label htmlFor='height'>Height (in cm)</label>
+                    <input id='height' type='text' />
+                </div>
+            </div>
+            <div className='calculate-btn'>
+                <button>Calculate BMI</button>
+            </div>
+            <div className='bmi-chart'>
+                <img src={img} width='500px' alt='chart-img' />
+            </div>
+            <div className='sub-header'>7 Day Data</div>
+            <div className='bmi-history-container'>
+                <div className='history-item'>
+                    <div className='heading'>BMI</div>
+                    <p className='info'>Weight</p>
+                    <p className='info'>Height</p>
+                    <p className='info'>Date</p>
+                </div>
+                <div className='history-item'>
+                    <div className='heading'>BMI</div>
+                    <p className='info'>Weight</p>
+                    <p className='info'>Height</p>
+                    <p className='info'>Date</p>
+                </div>
+                <div className='history-item'>
+                    <div className='heading'>BMI</div>
+                    <p className='info'>Weight</p>
+                    <p className='info'>Height</p>
+                    <p className='info'>Date</p>
+                </div>
+                <div className='history-item'>
+                    <div className='heading'>BMI</div>
+                    <p className='info'>Weight</p>
+                    <p className='info'>Height</p>
+                    <p className='info'>Date</p>
+                </div>
+            </div>
+            <div className='undo-btn'>
+                <button>Undo</button>
+            </div>
+        </div>
+    )
+}
+
+export default App
